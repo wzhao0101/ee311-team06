c53af3905a6d7e82dc25d42eb6c5b93c8029efa2
Wendy(Yuxin) Zhao <byhz780@aucklanduni.ac.nz>
2026-09-25 15:58:56 +1200

    Voltage Sesing code start

---
+++ Smarts/Firmware/VoltageSensing/VoltageSensing/main.c
@@ -0,0 +1,18 @@
+/*
+ * VoltageSensing.c
+ *
+ * Created: 25/09/2026 3:31:13 pm
+ * Author : byhz780
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
